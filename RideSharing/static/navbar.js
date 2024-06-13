be28278b7e9774f3117b2114eb0c76e582573592
shumit taher <shumitt@gmail.com>
2024-06-13 13:00:17 +0600

    active status fixed

---
+++ RideSharing/static/navbar.js
@@ -0,0 +1,28 @@
+function fetchActiveRoute() {
+
+    let activeroute = location.pathname  
+
+    return (activeroute)
+    
+}
+
+function updateActiveStatus() {
+    
+    let links = document.querySelectorAll(".nav-link")
+    let activeRoute = fetchActiveRoute()
+    
+    for (let index = 0; index < links.length-1; index++) {
+        
+        if (links[index].pathname==activeRoute) {
+            links[index].classList.add("active")
+            links[index].ariaCurrent = "page"
+            break
+        } else {
+            links[index].classList.remove("active")
+            links[index].removeAttribute("ariaCurrent")
+        }
+                
+        }
+}
+
+updateActiveStatus()
