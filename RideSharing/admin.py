bdffa420906a65ac7ea2cc5a619561e92de259b4
shumit taher <shumitt@gmail.com>
2024-06-20 16:04:31 +0600

    admin module built

--- RideSharing/admin.py
+++ RideSharing/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Locations, Trips
+
+
+class LocationsAdmin(admin.ModelAdmin):
+    list_display = ("id", "location_area", "location_name", "embarcation_text", "type")
 
 # Register your models here.
+admin.site.register(Locations, LocationsAdmin)
+admin.site.register(Trips)
