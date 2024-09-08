092bbdd664ebf09caa16b77c046357e74b249f74
shumit taher <shumitt@gmail.com>
2024-09-08 23:18:00 +0600

    navbar fix color change

--- RideSharing/static/navbar.js
+++ RideSharing/static/navbar.js
@@ -61,11 +61,11 @@ function updateActiveStatus() {
     document.addEventListener('scroll', ()=>{
 
         if (window.scrollY === 0) {
-            top_bar.classList.add("at_top");
+            top_bar.classList.add("at_top", "navbar-dark");
             top_bar.classList.remove("not_at_top");
         } else {
             top_bar.classList.add("not_at_top");
-            top_bar.classList.remove("at_top");
+            top_bar.classList.remove("at_top", "navbar-dark");
         }
 
     })
