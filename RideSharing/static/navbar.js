2ae290274d2a1e5f00ed8459a19441cbfd8fc614
shumit taher <shumitt@gmail.com>
2024-09-09 01:22:22 +0600

    colors

--- RideSharing/static/navbar.js
+++ RideSharing/static/navbar.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     updateActiveStatus()
 
+    if (location.pathname=='/') {
+        top_bar.classList.add("navbar-dark")
+    }
+
     unread_messagecount_space = document.getElementById('unread_messagecount_space')
 
     if (unread_messagecount_space) {
@@ -34,17 +38,10 @@ async function get_unread_counter() {
     }
 }
 
-function fetchActiveRoute() {
-
-    let activeroute = location.pathname
-    return (activeroute)
-    
-}
-
 function updateActiveStatus() {
     
     let links = top_bar.querySelectorAll(".nav-link")
-    let activeRoute = fetchActiveRoute()
+    let activeRoute = location.pathname
     
     links.forEach((link)=>{
         
@@ -59,15 +56,15 @@ function updateActiveStatus() {
     })
 
     document.addEventListener('scroll', ()=>{
-
-        if (window.scrollY === 0) {
-            top_bar.classList.add("at_top", "navbar-dark");
-            top_bar.classList.remove("not_at_top");
-        } else {
-            top_bar.classList.add("not_at_top");
-            top_bar.classList.remove("at_top", "navbar-dark");
-        }
-
+            
+            if (window.scrollY === 0) {
+                top_bar.classList.add("at_top", activeRoute=='/'?"navbar-dark":"_");
+                top_bar.classList.remove("not_at_top");
+            } else {
+                top_bar.classList.add("not_at_top");
+                top_bar.classList.remove("at_top", activeRoute=='/'?"navbar-dark":"_");
+            }
+            
     })
 
 
