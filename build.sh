9c8c689023b4f738d98918f11ea87fccf17e68d0
shumit taher <shumitt@gmail.com>
2024-09-22 16:13:32 +0600

    removed apt

--- build.sh
+++ build.sh
@@ -4,10 +4,6 @@ set -o errexit  # Exit on error
 # Upgrade pip
 pip install --upgrade pip
 
-# Install system dependencies
-sudo apt-get update
-sudo apt-get install -y pkg-config libcairo2-dev cmake
-
 # Install Python dependencies
 pip install -r requirements.txt
 
