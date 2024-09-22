bcc43b5bb5be2a9f43491c19f1eb9d529da0cc30
shumit taher <shumitt@gmail.com>
2024-09-22 15:39:42 +0600

    build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o errexit  # Exit on error
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Apply database migrations
+python manage.py migrate
