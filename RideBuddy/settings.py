286b0f92487c006e0543938211687de461bfc10f
shumit taher <shumitt@gmail.com>
2024-09-23 17:17:05 +0600

    changed debug to false

--- RideBuddy/settings.py
+++ RideBuddy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_0!9zo5u!l2(4+5xu)_sbj5=+e=5gr=asb=sd=9$e_&v4ahl#='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ridebuddy-hv9j.onrender.com', 'localhost', '127.0.0.1']
 
